9a196fffca0f52b5607e38585eef2426135ab31e
Aleksander Halvorsen Holthe <alekhh123@gmail.com>
2022-02-06 15:25:00 +0100

    Made threading work with prime numbers

--- P1/main.cpp
+++ P1/main.cpp
@@ -9,7 +9,7 @@ bool isPrime(int n) {
     return false;
   }
 
-  for (int i = 2; i < n/2; i++){
+  for (int i = 2; i < n; i++){
     if (n % i == 0){
       return false;
     }
@@ -18,20 +18,46 @@ bool isPrime(int n) {
   return true;
 }
 
+void getPrimesInRange(int start, int end, vector<int> *numberVector){
+
+  for (size_t i = start; i < end; i++){
+    if (isPrime(i))
+    {
+      numberVector->__emplace_back(i);
+    }
+  }
+}
+
 int main() {
-  vector<thread> threads;
 
   int startNumber = 0;
-  int endNumber = 100000;
-  int amountOfThreads = 2;
+  int endNumber = 500000;
+  int amountOfThreads = 6;
+  int splitPoint = (endNumber-startNumber)/amountOfThreads;
 
+  vector<int> numberVector[amountOfThreads];
+  vector<thread> threadVector;
 
-  for (int i = 0; i < 10; i++) {
-    threads.emplace_back([i] {                    // i is copied to the thread, do not capture i as reference (&i) as it might be freed before all the threads finishes.
-      cout << "Output from thread " << i << endl; // The output might be interleaved since the string, integer and newline are output in succession.
-    });
+  for (int i = 0; i < amountOfThreads; i++){
+    threadVector.emplace_back(getPrimesInRange, startNumber+(splitPoint*i), splitPoint + (splitPoint*i), &numberVector[i]);
   }
 
-  for (auto &thread : threads)
+for (auto &thread : threadVector)
     thread.join();
+
+/*
+  for(int i=0; i < numberVector[0].size(); i++)
+  cout << numberVector[0].at(i) << ' ';
+
+  cout << endl << "" << endl;
+
+  for(int i=0; i < numberVector[1].size(); i++)
+  cout << numberVector[1].at(i) << ' ';
+
+  cout << endl << "" << endl;
+
+  for(int i=0; i < numberVector[2].size(); i++)
+  cout << numberVector[2].at(i) << ' ';
+*/
+
 }
